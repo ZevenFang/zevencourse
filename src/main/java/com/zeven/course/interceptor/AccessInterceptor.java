94e10cf0a62c2b863e202e2a6778cb46aae64e79
zeven <fangfan1997@126.com>
2016-12-18 19:01:32 +0800

    fix JWT String empty argument.

--- src/main/java/com/zeven/course/interceptor/AccessInterceptor.java
+++ src/main/java/com/zeven/course/interceptor/AccessInterceptor.java
@@ -16,25 +16,31 @@ public class AccessInterceptor extends CrossDomainInterceptor {
         String path = request.getContextPath();
         String uri = request.getRequestURI();
         if (!uri.startsWith(path+"/Access")) {
-                Token token = new Token(request.getHeader("Authorization"));
-                if (token.getErr()==Token.ExpiredJwtError){
-                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED,
-                            "Authentication Failed: 认证过期");
-                    return false;
-                }
-                if (token.getErr()==Token.SignatureError){
-                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED,
-                            "Authentication Failed: 非法认证");
-                    return false;
-                }
-                boolean isAdmin = !token.getRole().equals("admin")&&uri.startsWith(path+"/Admin");
-                boolean isTeacher = !token.getRole().equals("teacher")&&uri.startsWith(path+"/Teacher");
-                boolean isStudent = !token.getRole().equals("student")&&uri.startsWith(path+"/Student");
-                if (isAdmin||isTeacher||isStudent) {
-                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED,
-                            "Authentication Failed: 无权访问");
-                    return false;
-                }
+            String auth = request.getHeader("Authorization");
+            if (auth==null){
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED,
+                        "Authentication Failed: Require Authorization");
+                return false;
+            }
+            Token token = new Token(auth);
+            if (token.getErr()==Token.ExpiredJwtError){
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED,
+                        "Authentication Failed: 认证过期");
+                return false;
+            }
+            if (token.getErr()==Token.SignatureError){
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED,
+                        "Authentication Failed: 非法认证");
+                return false;
+            }
+            boolean isAdmin = !token.getRole().equals("admin")&&uri.startsWith(path+"/Admin");
+            boolean isTeacher = !token.getRole().equals("teacher")&&uri.startsWith(path+"/Teacher");
+            boolean isStudent = !token.getRole().equals("student")&&uri.startsWith(path+"/Student");
+            if (isAdmin||isTeacher||isStudent) {
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED,
+                        "Authentication Failed: 无权访问");
+                return false;
+            }
         }
         return true;
     }
